6d28b8439fe7a714dc337f599f2363cb6f07745e
cecii27 <cecimouraramos1@gmail.com>
2021-11-23 15:46:18 -0300

    Colocando os arquivos da minha aplicação

---
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package ifrn.pi.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index(){
+		System.out.println("Chamou o método index");
+		return "redirect:/eventos";
+	}
+		
+}
+	
+
